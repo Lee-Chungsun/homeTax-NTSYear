a4b793d5dfcb5d41c07b2728e0b2640be02fddb0
Lee-Chungsun <55130795+Lee-Chungsun@users.noreply.github.com>
2019-09-11 15:26:21 +0900

    Create AdjustCtrl.java

---
+++ src/main/java/Adjustweb/AdjustCtrl.java
@@ -0,0 +1,19 @@
+public void ntsYearEndAdjust(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//Thread tr = new Thread();
+		StringBuilder log = new StringBuilder();
+		log.append("\n################################ revenue ntsYearEndAdjust Start ################################");
+		log.append("\n### ntsYearEndAdjust Force Recv ");
+		logger.debug(log);
+		
+		JSONParser jsonParser = new JSONParser();
+	  JSONObject jsonObject = (JSONObject) jsonParser.parse(readBody(request));
+    
+	  
+    Set<String> files = ftpFileList(param);
+    for(String file : files){
+	    param.put("FILE_NM", file);
+			if(file.equals(empId)){
+				result = ntsPdfSendApi(param);
+			}
+		}
+}
